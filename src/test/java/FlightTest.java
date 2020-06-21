c59a49d663e56130b5e4b92048322a09db793c8e
Victor <victorsetlhare@gmail.com>
2020-06-21 22:19:01 +0100

    flight test setup

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -12,7 +12,8 @@ public class FlightTest {
     @Before
     public void before(){
         flight = new Flight("Jet007", "London", "Edinburgh", "0700");
-        passenger1 new Passenger("tim");
-        passenger2 = new
+        passenger1 = new Passenger("Tim");
+        passenger2 = new Passenger("Jimmy");
+        passenger3 = new Passenger("Grace");
     }
 }
