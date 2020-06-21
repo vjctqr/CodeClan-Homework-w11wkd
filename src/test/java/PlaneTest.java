e5ca5c999a31a833c104c8d12eea21a409db12e7
Victor <victorsetlhare@gmail.com>
2020-06-21 21:27:59 +0100

    write planeType Test

--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PlaneTest {
 
@@ -7,5 +10,10 @@ public class PlaneTest {
     @Before
     public void before(){
         plane = new Plane(PlaneType.JET);
+    }
+
+    @Test
+    public void canGetCraft(){
+        assertEquals(PlaneType.JET, plane.getCraft());
     }
 }
