83a1afca1172c483db0dd5b94effe6da46e6920e
Victor <victorsetlhare@gmail.com>
2020-06-21 20:30:18 +0100

    zero bag count tested

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+
 public class Passenger {
 
-    public String name;
+    private String name;
+    private ArrayList<Bag> luggage;
 
     public Passenger(String name){
         this.name = name;
+        this.luggage = new ArrayList<Bag>();
     }
 
     public String getName(){
         return this.name;
+    }
+
+    public int bagCount(){
+        return this.luggage.size();
     }
 }
