2669f8c3ee5ce78ceca99b47dc8c334672bad2ac
Victor <victorsetlhare@gmail.com>
2020-06-21 22:17:37 +0100

    flight calss created

---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+
+public class FlightTest {
+
+    Flight flight;
+    Passenger passenger1;
+    Passenger passenger2;
+    Passenger passenger3;
+    Plane plane;
+
+
+    @Before
+    public void before(){
+        flight = new Flight("Jet007", "London", "Edinburgh", "0700");
+        passenger1 new Passenger("tim");
+        passenger2 = new
+    }
+}
